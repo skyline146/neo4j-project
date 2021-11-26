1289d3314bd983425acd7f7bb79c7d922ee8b3a3
Dmitry <dmitrilevchenko23@gmail.com>
2021-11-26 14:56:58 +0200

    fixes + way length output

--- task1/index.js
+++ task1/index.js
@@ -11,28 +11,56 @@ const runQuery = async (query) => {
     return result;
 }
 
-const firstBusStops = await runQuery(`
-MATCH p = (begin)-[:LENGTH *0..50]-(finish)
-WHERE begin.name = 'Красноармейская' AND finish.name = 'м/н Шуменский'
-RETURN length(p)+1
-`);
+const runMultiQuery = async (query) => {
+    const session = dbConnection.session();
+    const dbResult = await session.run(query);
+    const result = [];
+    for (let i = 0; i < dbResult.records.length; i++) {
+        result.push(dbResult.records[i].get(0));
+    }
+    session.close();
+    return result;
+}
 
-console.log(`17 bus stops count: ${firstBusStops}`);
+function sumLength(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.segments.length; i++) {
+        sum += arr.segments[i].relationship.properties.data;
+    }
+    return sum;
+}
 
-const secondBusStops = await runQuery(`
-MATCH p = (begin)-[:LENGTH *0..50]-(finish)
-WHERE begin.name = '3-я Фабрика ХБК' AND finish.name = 'м/н Таврический'
-RETURN length(p)+1
-`);
+const runQueryParams = async (start, end) => {
+    return await runQuery(`
+        MATCH p = (begin)-[:LENGTH *0..50]-(finish)
+        WHERE begin.name = ${start} AND finish.name = ${end}
+        RETURN p
+    `);
+} 
 
-console.log(`14 bus stops count: ${secondBusStops}`);
+const arrayBusStops = async (bus = '') => {
+    return await runMultiQuery(`
+        MATCH (n:Stops${bus})
+        RETURN n.name
+    `);
+}
 
-const thirdBusStops = await runQuery(`
-MATCH p = (begin)-[:LENGTH *0..50]-(finish)
-WHERE begin.name = 'Набережная' AND finish.name = 'пр. Сенявина'
-RETURN length(p)+1
-`);
+const firstBusStops = await runQueryParams("'Красноармейская'", "'м/н Шуменский'");
+const firstBusStopsArray = await arrayBusStops();
 
-console.log(`6 bus stops count: ${thirdBusStops}`);
+console.log(`17 bus stops count: ${firstBusStops.length+1}, way length: ${sumLength(firstBusStops)}`);
+// console.log(`Bus way: ${firstBusStopsArray.join(" --> ")}\n`);
+
+const secondBusStops = await runQueryParams("'3-я Фабрика ХБК'", "'м/н Таврический'");
+const secondBusStopsArray = await arrayBusStops('2');
+
+console.log(`14 bus stops count: ${secondBusStops.length+1}, way length: ${sumLength(secondBusStops)}`);
+// console.log(`Bus way: ${secondBusStopsArray.join(" --> ")}\n`);
+
+const thirdBusStops = await runQueryParams("'Набережная'", "'пр. Сенявина'");
+const thirdBusStopsArray = await arrayBusStops('3');
+
+console.log(`6 bus stops count: ${thirdBusStops.length+1}, way length: ${sumLength(thirdBusStops)}`);
+// console.log(`Bus way: ${thirdBusStopsArray.join(" --> ")}\n`);
 
 dbConnection.close();
